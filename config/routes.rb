cc07c159e3641a1e3749f4f0a499aaaca380a895
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-05-25 15:03:37 -0300

    restaurant view finished - restaurant and review models and controller finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants do
+  resources :restaurants, except: %i[destroy edit update] do
     resources :reviews, only: %i[new create]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_163924) do
+ActiveRecord::Schema.define(version: 2022_05_25_165942) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 2022_05_25_163924) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "rating"
+    t.integer "rating"
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "restaurant_id"
   end
 
 end
